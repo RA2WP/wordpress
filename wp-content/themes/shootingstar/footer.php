17d96bdc2c016171fcaa69034367c0e1e11c370e
Guangling <Guangling@GuanglingChen>
2015-12-21 15:37:20 +0200

    Add plugins for photo gallery.

--- wp-content/themes/shootingstar/footer.php
+++ wp-content/themes/shootingstar/footer.php
@@ -17,7 +17,7 @@
           <div style="width: 500px">
               <br />
 	  <font style="color: #E7E7E7; font-size: 14px">&nbsp;&nbsp;&nbsp;&nbsp;© ELI-NP RA2. All rights reserved.&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</font>
-          <font style="color: #E7E7E7; font-size: 14px"><a href="<?php echo get_site_url(); ?>/webmasters">Webmasters</a> | <a href="<?php echo get_site_url(); ?>"> Feedback</a> | <a href="<?php echo get_site_url(); ?>/wp-admin/"> Login</a></font><br><br><br>
+          <font style="color: #E7E7E7; font-size: 14px"><a href="<?php echo get_site_url(); ?>/webmasters">Webmasters</a> | <a href="<?php echo get_site_url(); ?>"> Feedback</a> </font><br><br><br>
           </div>
     </div>    
     <div class="footer-widget-area footer-widget-area-2">
