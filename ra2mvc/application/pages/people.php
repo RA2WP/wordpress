798955def1f4774043c4ce9124c0911947f5cf68
Guangling <Guangling@GuanglingChen>
2015-12-22 17:10:13 +0200

    File download.

--- ra2mvc/application/pages/people.php
+++ ra2mvc/application/pages/people.php
@@ -13,22 +13,24 @@
   <tbody>
     <tr>
       <td><a href="<?php echo get_site_url(); ?> /calin-ur/">Calin Ur</a></td>
-      <td>#</td>
+      <td>##-##</td>
       <td><a href="mailto:calin.ur@eli-np.ro">calin.ur@eli-np.ro</a></td>
-      <td>Head</td>
+      <td>Head of Department</td>
       <td>106</td>
     </tr>
     <tr>
       <td><a href="<?php echo get_site_url(); ?> /ra2-people/">Dorina Aranghel</a></td>
-      <td>#</td>
+      <td>##-##</td>
       <td><a href="mailto:dorina.aranghel@eli-np.ro">dorina.aranghel@eli-np.ro</a></td>
-      <td>Engineer</td>
+      <td>Researcher</td>
+      <td>##</td>
     </tr>
     <tr>
       <td><a href="<?php echo get_site_url(); ?> /ra2-people/">Valeriu Buznea</a></td>
       <td>+40723677681</td>
       <td><a href="mailto:valeriu.buznea@eli-np.ro">valeriu.buznea@eli-np.ro</a></td>
-      <td>Engineer</td>
+      <td>Technician</td>
+      <td>##</td>
     </tr>
     <tr>
       <td><a href="<?php echo get_site_url(); ?> /guangling-chen/">Guangling Chen</a></td>
@@ -39,46 +41,52 @@
     </tr>
     <tr>
       <td><a href="<?php echo get_site_url(); ?> /ra2-people/">Marcel Conde</a></td>
-      <td>#</td>
+      <td>##-##</td>
       <td><a href="mailto:marcel.conde@eli-np.ro">marcel.conde@eli-np.ro</a></td>
-      <td>Engineer</td>
+      <td>Technician</td>
+      <td>##</td>
     </tr>
     <tr>
       <td><a href="<?php echo get_site_url(); ?> /ra2-people/">Teodor Dogaru</a></td>
-      <td>#</td>
+      <td>##-##</td>
       <td><a href="mailto:teodor.dogaru@eli-np.ro">teodor.dogaru@eli-np.ro</a></td>
       <td>Engineer</td>
+      <td>##</td>
     </tr>
     <tr>
       <td><a href="<?php echo get_site_url(); ?> /ra2-people/">Ioan Garagaianu</a></td>
-      <td>#</td>
+      <td>##-##</td>
       <td><a href="mailto:ioan.garagaianu@eli-np.ro">ioan.garagaianu@eli-np.ro</a></td>
-      <td>Engineer</td>
+      <td>Technician</td>
+      <td>##</td>
     </tr>
     <tr>
       <td><a href="<?php echo get_site_url(); ?> /ra2-people/">Violeta Iancu</a></td>
-      <td>#</td>
+      <td>##-##</td>
       <td><a href="mailto:violeta.iancu@eli-np.ro">violeta.iancu@eli-np.ro</a></td>
-      <td>Engineer</td>
+      <td>Researcher</td>
       <td>106</td>
     </tr>
     <tr>
       <td><a href="<?php echo get_site_url(); ?> /ra2-people/">Iani Mitu</a></td>
-      <td>#</td>
+      <td>##-##</td>
       <td><a href="mailto:iani.mitu@eli-np.ro">iani.mitu@eli-np.ro</a></td>
       <td>Engineer</td>
+      <td>##</td>
     </tr>
     <tr>
       <td><a href="<?php echo get_site_url(); ?> /ra2-people/">Cristian Paun</a></td>
       <td>0721441529</td>
       <td><a href="mailto:cristian.paun@eli-np.ro">cristian.paun@eli-np.ro</a></td>
-      <td>Engineer</td>
+      <td>Technician</td>
+      <td>##</td>
     </tr>
     <tr>
       <td><a href="<?php echo get_site_url(); ?> /ra2-people/">Cristian Petcu</a></td>
       <td>+40723677681</td>
       <td><a href="mailto:cristian.petcu@eli-np.ro">cristian.petcu@eli-np.ro</a></td>
       <td>Engineer</td>
+      <td>##</td>
     </tr>
     <tr>
       <td><a href="<?php echo get_site_url(); ?> /cristian-savlovschi/">Cristian Savlovschi</a></td>
@@ -89,56 +97,69 @@
     </tr>
     <tr>
       <td><a href="<?php echo get_site_url(); ?> /gabriel-suliman/">Gabriel Suliman</a></td>
-      <td>#</td>
+      <td>##-##</td>
       <td><a href="mailto:gabriel.suliman@eli-np.ro">gabriel.suliman@eli-np.ro</a></td>
-      <td>Engineer</td>
+      <td>Researcher</td>
       <td>205</td>
     </tr>
     <tr>
       <td><a href="<?php echo get_site_url(); ?> /ra2-people/">Bogdan Tatulea</a></td>
-      <td>#</td>
+      <td>##-##</td>
       <td><a href="mailto:bogdan.tatulea@eli-np.ro">bogdan.tatulea@eli-np.ro</a></td>
       <td>Engineer</td>
+      <td>##</td>
     </tr>
     <tr>
       <td><a href="<?php echo get_site_url(); ?> /ra2-people/">Marian Toma</a></td>
-      <td>#</td>
+      <td>##-##</td>
       <td><a href="mailto:marian.toma@eli-np.ro">marian.toma@eli-np.ro</a></td>
       <td>Engineer</td>
       <td>205</td>
     </tr>
     <tr>
       <td><a href="<?php echo get_site_url(); ?> /ra2-people/">Ewa Tracz</a></td>
-      <td>#</td>
+      <td>##-##</td>
       <td><a href="mailto:ewa.tracz@eli-np.ro">ewa.tracz@eli-np.ro</a></td>
       <td>Engineer</td>
       <td>106</td>
     </tr>
     <tr>
       <td><a href="<?php echo get_site_url(); ?> /ra2-people/">Piotr Tracz</a></td>
-      <td>#</td>
+      <td>##-##</td>
       <td><a href="mailto:piotr.tracz@eli-np.ro">piotr.tracz@eli-np.ro</a></td>
       <td>Engineer</td>
       <td>205</td>
     </tr>
     <tr>
       <td><a href="<?php echo get_site_url(); ?> /ra2-people/">Gabriel Turturica</a></td>
-      <td>#</td>
+      <td>##-##</td>
       <td><a href="mailto:gabriel.turturica@eli-np.ro">gabriel.turturica@eli-np.ro</a></td>
       <td>Engineer</td>
+      <td>##</td>
     </tr>
     <tr>
       <td><a href="<?php echo get_site_url(); ?> /ra2-people/">Emil Udup</a></td>
-      <td>#</td>
+      <td>##-##</td>
       <td><a href="mailto:emil.udup@eli-np.ro">emil.udup@eli-np.ro</a></td>
       <td>Engineer</td>
+      <td>##</td>
     </tr>
     <tr>
       <td><a href="<?php echo get_site_url(); ?> /ra2-people/">Mihai Ciubancan</a></td>
-      <td>#</td>
+      <td>##-##</td>
       <td><a href="mailto:mihai.ciubancan@eli-np.ro">mihai.ciubancan@eli-np.ro</a></td>
       <td>Engineer</td>
+      <td>##</td>
     </tr>
     
+  <tfoot>
+    <tr>
+      <th>Name</th>
+      <th>Phone</th>
+      <th>Email</th>
+      <th>Position</th>
+      <th>Room</th>
+    </tr>
+  </tfoot>
   </tbody>
 </table>
