7f3fb14cd6ca8e39c5ae8fe5b15e5a3d2020d36f
Guangling <Guangling@GuanglingChen>
2016-01-06 19:09:51 +0200

    delete download menu.

--- ra2mvc/application/pages/people.php
+++ ra2mvc/application/pages/people.php
@@ -11,142 +11,143 @@
     </tr>
   </thead>
   <tbody>
-    <tr>
-      <td><a href="<?php echo get_site_url(); ?> /calin-ur/">Calin Alexandru Ur</a></td>
-      <td>##-##</td>
+    <tr>        
+      <td><a href="<?php if ( is_user_logged_in() ) {echo get_site_url().'/calin-ur/';} else {echo get_site_url().'/ra2-people/';} ?> ">Calin Alexandru Ur</a></td>
+      <td>3804</td>
       <td><a href="mailto:calin.ur@eli-np.ro">calin.ur@eli-np.ro</a></td>
       <td>Head of Department</td>
       <td>106</td>
     </tr>
+
     <tr>
-      <td><a href="<?php echo get_site_url(); ?> /ra2-people/">Dorina Aranghel</a></td>
-      <td>##-##</td>
+      <td><a href="<?php if ( is_user_logged_in() ) {echo get_site_url().'/dorina-aranghel/';} else {echo get_site_url().'/ra2-people/';} ?> ">Dorina Aranghel</a></td>
+      <td> </td>
       <td><a href="mailto:dorina.aranghel@eli-np.ro">dorina.aranghel@eli-np.ro</a></td>
       <td>Researcher</td>
       <td>##</td>
     </tr>
     <tr>
-      <td><a href="<?php echo get_site_url(); ?> /ra2-people/">Valeriu Buznea</a></td>
-      <td>+40723677681</td>
+      <td><a href="<?php if ( is_user_logged_in() ) {echo get_site_url().'/valeriu-buznea/';} else {echo get_site_url().'/ra2-people/';} ?> ">Valeriu Buznea</a></td>
+      <td>3803</td>
       <td><a href="mailto:valeriu.buznea@eli-np.ro">valeriu.buznea@eli-np.ro</a></td>
       <td>Technician</td>
       <td>##</td>
     </tr>
     <tr>
-      <td><a href="<?php echo get_site_url(); ?> /guangling-chen/">Guangling Chen</a></td>
-      <td>+40 701 143 304</td>
+      <td><a href="<?php if ( is_user_logged_in() ) {echo get_site_url().'/guangling-chen/';} else {echo get_site_url().'/ra2-people/';} ?> ">Guangling Chen</a></td>
+      <td>3945</td>
       <td><a href="mailto:guangling.chen@eli-np.ro">guangling.chen@eli-np.ro</a></td>
       <td>Engineer</td>
       <td>205</td>
     </tr>
     <tr>
-      <td><a href="<?php echo get_site_url(); ?> /ra2-people/">Marcel Conde</a></td>
-      <td>##-##</td>
+      <td><a href="<?php if ( is_user_logged_in() ) {echo get_site_url().'/marcel-conde/';} else {echo get_site_url().'/ra2-people/';} ?> ">Marcel Conde</a></td>
+      <td>3803</td>
       <td><a href="mailto:marcel.conde@eli-np.ro">marcel.conde@eli-np.ro</a></td>
       <td>Technician</td>
       <td>##</td>
     </tr>
     <tr>
-      <td><a href="<?php echo get_site_url(); ?> /ra2-people/">Teodor Dogaru</a></td>
-      <td>##-##</td>
+      <td><a href="<?php if ( is_user_logged_in() ) {echo get_site_url().'/teodor-dogaru/';} else {echo get_site_url().'/ra2-people/';} ?> ">Teodor Dogaru</a></td>
+      <td>3976</td>
       <td><a href="mailto:teodor.dogaru@eli-np.ro">teodor.dogaru@eli-np.ro</a></td>
       <td>Engineer</td>
       <td>##</td>
     </tr>
     <tr>
-      <td><a href="<?php echo get_site_url(); ?> /ra2-people/">Ioan Garagaianu</a></td>
-      <td>##-##</td>
+      <td><a href="<?php if ( is_user_logged_in() ) {echo get_site_url().'/ioan-garagaianu/';} else {echo get_site_url().'/ra2-people/';} ?> ">Ioan Garagaianu</a></td>
+      <td>3803</td>
       <td><a href="mailto:ioan.garagaianu@eli-np.ro">ioan.garagaianu@eli-np.ro</a></td>
       <td>Technician</td>
       <td>##</td>
     </tr>
     <tr>
-      <td><a href="<?php echo get_site_url(); ?> /ra2-people/">Violeta Iancu</a></td>
-      <td>##-##</td>
+      <td><a href="<?php if ( is_user_logged_in() ) {echo get_site_url().'/violeta-iancu/';} else {echo get_site_url().'/ra2-people/';} ?> ">Violeta Iancu</a></td>
+      <td>3951</td>
       <td><a href="mailto:violeta.iancu@eli-np.ro">violeta.iancu@eli-np.ro</a></td>
       <td>Researcher</td>
       <td>106</td>
     </tr>
     <tr>
-      <td><a href="<?php echo get_site_url(); ?> /ra2-people/">Iani Mitu</a></td>
-      <td>##-##</td>
+      <td><a href="<?php if ( is_user_logged_in() ) {echo get_site_url().'/iani-mitu/';} else {echo get_site_url().'/ra2-people/';} ?> ">Iani Mitu</a></td>
+      <td>3975</td>
       <td><a href="mailto:iani.mitu@eli-np.ro">iani.mitu@eli-np.ro</a></td>
       <td>Engineer</td>
       <td>##</td>
     </tr>
     <tr>
-      <td><a href="<?php echo get_site_url(); ?> /ra2-people/">Cristian Paun</a></td>
-      <td>0721441529</td>
+      <td><a href="<?php if ( is_user_logged_in() ) {echo get_site_url().'/cristian-paun/';} else {echo get_site_url().'/ra2-people/';} ?> ">Cristian Paun</a></td>
+      <td>3803</td>
       <td><a href="mailto:cristian.paun@eli-np.ro">cristian.paun@eli-np.ro</a></td>
       <td>Technician</td>
       <td>##</td>
     </tr>
     <tr>
-      <td><a href="<?php echo get_site_url(); ?> /ra2-people/">Cristian Petcu</a></td>
-      <td>+40723677681</td>
+      <td><a href="<?php if ( is_user_logged_in() ) {echo get_site_url().'/cristian-petcu/';} else {echo get_site_url().'/ra2-people/';} ?> ">Cristian Petcu</a></td>
+      <td>3803</td>
       <td><a href="mailto:cristian.petcu@eli-np.ro">cristian.petcu@eli-np.ro</a></td>
       <td>Engineer</td>
       <td>##</td>
     </tr>
     <tr>
-      <td><a href="<?php echo get_site_url(); ?> /cristian-savlovschi/">Cristian Savlovschi</a></td>
-      <td>0040724799623</td>
+      <td><a href="<?php if ( is_user_logged_in() ) {echo get_site_url().'/cristian-savlovschi/';} else {echo get_site_url().'/ra2-people/';} ?> ">Cristian Savlovschi</a></td>
+      <td>3944</td>
       <td><a href="mailto:cristian.savlovschi@eli-np.ro">cristian.savlovschi@eli-np.ro</a></td>
       <td>Engineer</td>
       <td>205</td>
     </tr>
     <tr>
-      <td><a href="<?php echo get_site_url(); ?> /gabriel-suliman/">Gabriel Suliman</a></td>
-      <td>##-##</td>
+      <td><a href="<?php if ( is_user_logged_in() ) {echo get_site_url().'/gabriel-suliman/';} else {echo get_site_url().'/ra2-people/';} ?> ">Gabriel Suliman</a></td>
+      <td>3943</td>
       <td><a href="mailto:gabriel.suliman@eli-np.ro">gabriel.suliman@eli-np.ro</a></td>
       <td>Researcher</td>
       <td>205</td>
     </tr>
     <tr>
-      <td><a href="<?php echo get_site_url(); ?> /ra2-people/">Bogdan Tatulea</a></td>
-      <td>##-##</td>
+      <td><a href="<?php if ( is_user_logged_in() ) {echo get_site_url().'/bogdan-tatulea/';} else {echo get_site_url().'/ra2-people/';} ?> ">Bogdan Tatulea</a></td>
+      <td>3803</td>
       <td><a href="mailto:bogdan.tatulea@eli-np.ro">bogdan.tatulea@eli-np.ro</a></td>
       <td>Engineer</td>
       <td>##</td>
     </tr>
     <tr>
-      <td><a href="<?php echo get_site_url(); ?> /ra2-people/">Marian Toma</a></td>
-      <td>##-##</td>
+      <td><a href="<?php if ( is_user_logged_in() ) {echo get_site_url().'/marian-toma/';} else {echo get_site_url().'/ra2-people/';} ?> ">Marian Toma</a></td>
+      <td>3945</td>
       <td><a href="mailto:marian.toma@eli-np.ro">marian.toma@eli-np.ro</a></td>
       <td>Engineer</td>
       <td>205</td>
     </tr>
     <tr>
-      <td><a href="<?php echo get_site_url(); ?> /ra2-people/">Ewa Tracz</a></td>
-      <td>##-##</td>
+      <td><a href="<?php if ( is_user_logged_in() ) {echo get_site_url().'/ewa-tracz/';} else {echo get_site_url().'/ra2-people/';} ?> ">Ewa Tracz</a></td>
+      <td>3951</td>
       <td><a href="mailto:ewa.tracz@eli-np.ro">ewa.tracz@eli-np.ro</a></td>
       <td>Engineer</td>
       <td>106</td>
     </tr>
     <tr>
-      <td><a href="<?php echo get_site_url(); ?> /ra2-people/">Piotr Tracz</a></td>
-      <td>##-##</td>
+      <td><a href="<?php if ( is_user_logged_in() ) {echo get_site_url().'/piotr-tracz/';} else {echo get_site_url().'/ra2-people/';} ?> ">Piotr Tracz</a></td>
+      <td>3945</td>
       <td><a href="mailto:piotr.tracz@eli-np.ro">piotr.tracz@eli-np.ro</a></td>
       <td>Engineer</td>
       <td>205</td>
     </tr>
     <tr>
-      <td><a href="<?php echo get_site_url(); ?> /ra2-people/">Gabriel Turturica</a></td>
-      <td>##-##</td>
+      <td><a href="<?php if ( is_user_logged_in() ) {echo get_site_url().'/gabriel-turturica/';} else {echo get_site_url().'/ra2-people/';} ?> ">Gabriel Turturica</a></td>
+      <td>3943</td>
       <td><a href="mailto:gabriel.turturica@eli-np.ro">gabriel.turturica@eli-np.ro</a></td>
       <td>Engineer</td>
       <td>##</td>
     </tr>
     <tr>
-      <td><a href="<?php echo get_site_url(); ?> /ra2-people/">Emil Udup</a></td>
-      <td>##-##</td>
+      <td><a href="<?php if ( is_user_logged_in() ) {echo get_site_url().'/emil-udup/';} else {echo get_site_url().'/ra2-people/';} ?> ">Emil Udup</a></td>
+      <td>3803</td>
       <td><a href="mailto:emil.udup@eli-np.ro">emil.udup@eli-np.ro</a></td>
       <td>Engineer</td>
       <td>##</td>
     </tr>
     <tr>
-      <td><a href="<?php echo get_site_url(); ?> /ra2-people/">Mihai Ciubancan</a></td>
-      <td>##-##</td>
+      <td><a href="<?php if ( is_user_logged_in() ) {echo get_site_url().'/mihai-ciubancan/';} else {echo get_site_url().'/ra2-people/';} ?> ">Mihai Ciubancan</a></td>
+      <td>3502</td>
       <td><a href="mailto:mihai.ciubancan@eli-np.ro">mihai.ciubancan@eli-np.ro</a></td>
       <td>Engineer</td>
       <td>##</td>
